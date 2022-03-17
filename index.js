64db2e17c0a8cde2876e37476c4fe72f35fbbc0b
Thiago Cavalcante <thiagocavalcante@zup.com.br>
2022-03-17 16:28:48 -0300

    Get app port from env variable in Dockerfile

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express = require('express')
+const express = require("express");
 let app = express();
 
 app.use(express.static("."));
 
-app.get("/", (req,res)=>{
-    res.sendFile(__dirname + '/index.html')
-})
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
 
-app.listen("3000", ()=>{
-    console.log("Server is listening on port 3000")
-})
+app.listen(process.env.PORT, () => {
+  console.log("Server is listening on port 3000");
+});
